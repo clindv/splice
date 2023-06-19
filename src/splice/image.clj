0e8b01602a2c5c5719926e68ccfcd0e06eeb37ca
clindv <clindv@protonmail.com>
2023-06-19 14:39:30 +0800

    retouch sunder

--- src/splice/image.clj
+++ src/splice/image.clj
@@ -1,10 +1,10 @@
 (ns splice.image
-  (:import (java.io File
-                    FileFilter)
+  (:import (java.io File)
+           (java.awt image.BufferedImage)
            (javax.imageio ImageIO))
   (:require [splice.param :as param])
   (:gen-class))
-(defn suture [folder images suffix]
+(defn suture [images suffix ^File folder]
   ())
 (defn sunder [file-tree]
   (let [{folders true, files false} (group-by (comp map? val) file-tree)
@@ -12,10 +12,10 @@
         (group-by #(not-every? false? (map (fn [suffix] (.endsWith (.getName (key %)) suffix))
                                            param/real-suffix)) files)]
     (case [(boolean @param/real) (boolean @param/lossy)]
-      [false false] (do (suture (ffirst file-tree) real (first param/real-suffix))
-                        (suture (ffirst file-tree) lossy (first param/lossy-suffix)))
-      [true false] (suture (ffirst file-tree) files (first param/real-suffix))
-      [false true] (suture (ffirst file-tree) files (first param/lossy-suffix))
-      [true true] (do (suture (ffirst file-tree) files (first param/real-suffix))
-                      (suture (ffirst file-tree) files (first param/lossy-suffix))))
+      [false false] (do (suture real (ffirst file-tree) (first param/real-suffix))
+                        (suture lossy (ffirst file-tree) (first param/lossy-suffix)))
+      [true false] (suture files (ffirst file-tree) (first param/real-suffix))
+      [false true] (suture files (ffirst file-tree) (first param/lossy-suffix))
+      [true true] (do (suture files (ffirst file-tree) (first param/real-suffix))
+                      (suture files (ffirst file-tree) (first param/lossy-suffix))))
     (if (empty? folders) nil (recur (val (first folders))))))
